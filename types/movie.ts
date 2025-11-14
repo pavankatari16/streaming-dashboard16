38e2c107e5b9b833b988fb839ed7248c1b6a7180
K Pavan Kumar <pavan.katari3420@gmail.com>
2025-11-14 13:35:39 +0000

    fix: recreate Next.js project and project structure

--- types/movie.ts
+++
@@ -1,9 +0,0 @@
-export interface Movie {
-	id: number
-	title: string
-	poster_path?: string | null
-	backdrop_path?: string | null
-	overview?: string
-	vote_average?: number
-	release_date?: string
-}
